f30f9a2836e3b4441bcac525d3b0a0716d6f3fc5
Aarti Koli <aartikoli09@gmail.com>
2023-06-13 18:22:35 +0530

    Recomposition, State

--- JetpackComposeApplication/app/src/main/java/com/example/jetpackcomposeapplication/DynamicContentActivity.kt
+++ JetpackComposeApplication/app/src/main/java/com/example/jetpackcomposeapplication/DynamicContentActivity.kt
@@ -3,38 +3,62 @@ package com.example.jetpackcomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
-
-var nameList: List<String> = listOf("Jack","Wendy", "Janice", "Saurabh", "Eben")
 class DynamicContentActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingList(names = nameList)
+            DynamicScreen()
         }
     }
 }
+
 @Composable
-fun GreetingList(names: List<String>){
-    Column {
-        for (name in names){
-            Greeting(name = name)
-        }
+fun DynamicScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        GreetingList()
     }
 }
+
+@Composable
+fun GreetingList() {
+    val greetingListState =
+        remember { mutableStateListOf<String>("Jack", "Wendy", "Janice", "Eben") }
+    for (name in greetingListState) {
+        Greeting(name = name)
+    }
+    Button(onClick = { greetingListState.add("New name..") }) {
+        Text(text = "Add new member")
+    }
+}
+
 @Composable
 fun Greeting(name: String) {
     Text(
-        text = "Hi $name"
+        text = "Hi $name",
+        style = MaterialTheme.typography.headlineMedium
     )
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreviewDynamicContentActivity() {
-    GreetingList(names = nameList)
+    DynamicScreen()
 }
